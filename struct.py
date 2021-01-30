3c5a653e466def3ce499ff76cc16d9c9c115ce26
AooRobot <36130354+AooRobot@users.noreply.github.com>
2021-01-30 17:33:18 -0600

    struct, iterators, generators

---
+++ struct.py
@@ -0,0 +1,89 @@
+# python struct
+
+class Employee:
+    pass
+
+john = Employee()
+john.name = 'John Doe'
+john.dept = 'computer lab'
+john.salary = 1800
+
+# iterators
+s = 'abc'
+for i in s:
+    print(i)
+
+it = iter(s)
+print(next(it))
+print(next(it))
+print(next(it))
+
+# iterator in class
+class Reverse:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse([1,2,3])
+
+container = iter(rev)
+print(container)
+
+for char in rev:
+    # for statement call rev.__iter__ then call __next__
+    print(char)
+
+# newbie
+def reverse(data):
+    index = len(data)
+    re_data = []
+    for i in range(index):
+        if index == 0:
+            raise StopIteration
+        index = index - 1
+        re_data.insert(i, data[index])
+    return re_data
+
+print(reverse("spam"))
+print(reverse('123'))
+
+
+# Generators 
+# a simple and powerful tool for creating iterators
+# use "yield" statement whenever want to return data
+
+def reverse_re(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+for char in reverse_re("golf"):
+    print(char)
+
+# Generator expressions
+print_sum = sum(i*i for i in range(10))  # sum of squares
+print(print_sum)
+
+xvec = [10, 20, 30]
+yvec = [7, 5, 3]
+vec_sum = sum(x*y for x,y in zip(xvec, yvec))  # dot product
+print(vec_sum)
+
+from math import pi, sin
+sine_table = {x : sin(x*pi/180) for x in range(0, 91)}
+
+# unique_words = set(word for line in page for word in line.split())
+
+# valedictorian = max((student.gpa, student.name) for sutdent in graduates)
+
+data = 'golf'
+l = list(data[i] for i in range(len(data)-1, -1, -1))
+print(l)
